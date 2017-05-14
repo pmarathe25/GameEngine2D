fdc8bd07e320fd6c47e50afe32b7e1ba04983ec4
Pranav Marathe <pmarathe25@gmail.com>
2017-05-14 17:34:52 -0400

    Miscellaneous bug fixes.

--- test/tester.cpp
+++ test/tester.cpp
@@ -13,7 +13,7 @@ class Benchmark {
     public:
         Benchmark() {};
         void displayRealtimePerformanceInformation() {
-            std::cout << "\rCurrent Framerate: " << std::setw(6) << framerate << " fps\tTime elapsed: " << std::setw(8) << totalFrameTime << " seconds\tNumber of Frames: " << numFrames << std::flush;
+            std::cout << "\rCurrent Framerate: " << std::setw(4) << framerate << " fps\tTime elapsed: " << std::setw(8) << totalFrameTime << " seconds\tNumber of Frames: " << numFrames << std::flush;
         }
         void displayFramerateInformation() {
             std::cout << "Minimum Framerate: " << minFramerate << " fps\tAverage Framerate: " << (numFrames / totalFrameTime) << " fps\tMaximium Framerate: " << maxFramerate << " fps" << std::endl;
@@ -51,14 +51,14 @@ int main() {
     // Create clock
     sf::Clock clock;
     // Create some entities.
-    for (int i = 0; i < 50000; ++i) {
+    for (int i = 0; i < 150000; ++i) {
         int temp = entityManager.createEntity();
         renderSystem.addComponent(temp, RenderComponent(resourceManager.getTexture("player.png")));
         physicsSystem.addComponent(temp, PhysicsComponent(sf::Vector2f(1000 * (float(rand()) / RAND_MAX - 0.5), 1000
             * (float(rand()) / RAND_MAX - 0.5)), sf::Vector2f(1000 * (float(rand()) / RAND_MAX - 0.5), 1000
             * (float(rand()) / RAND_MAX - 0.5))));
     }
-    std::cout << "Components added. Time required: " << clock.getElapsedTime().asMilliseconds() << " milliseconds." << std::endl;
+    std::cout << "Entities created and components added. Time required: " << clock.getElapsedTime().asMilliseconds() << " milliseconds." << std::endl;
     // Remove some entities.
     // clock.restart();
     // for (int i = 49000; i >= 0; --i) {
